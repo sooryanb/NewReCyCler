f4ec9f09d7b944e99e416d506cbec2bf54e8b228
sooryanarayanan <sooryanarayanan@qburst.com>
2021-05-03 21:48:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RecyclerView-kotlin
+Here I have shown how to implement RecyclerView in Kotlin
+I have used one of the best approach to add ClickListner to RecyclerView
---
+++ app/src/main/java/com/example/newrecycler/CarAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.newrecycler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_item_view.view.*
+
+class CarAdapter(var clickListner: OnCarItemClickListner) :
+    ListAdapter<Cars, CarViewHolder>(CarDiffUtil()){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
+        lateinit var carViewHolder : CarViewHolder
+        carViewHolder = CarViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_item_view,parent,false ))
+        return carViewHolder
+    }
+
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+//        holder.carName?.text =  items.get(position).name
+//        holder.carDescription?.text = items.get(position).description
+//        holder.carLogo.setImageResource(items.get(position).logo)
+        holder.initialize(getItem(position),clickListner)
+
+    }
+}
+
+class CarViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+    var carName = itemView.carname
+    var carDescription = itemView.cardescription
+    var carLogo = itemView.carlogo
+
+    fun initialize(item: Cars, action:OnCarItemClickListner){
+        carName.text = item.name
+        carDescription.text = item.description
+        carLogo.setImageResource(item.logo)
+
+        itemView.setOnClickListener{
+            action.onItemClick(item,adapterPosition)
+        }
+
+    }
+
+}
+
+interface OnCarItemClickListner{
+    fun onItemClick(item: Cars, position: Int)
+}
+
+class CarDiffUtil: DiffUtil.ItemCallback<Cars>(){
+    override fun areItemsTheSame(oldItem: Cars, newItem: Cars): Boolean {
+        return oldItem.name === newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: Cars, newItem: Cars): Boolean {
+        return oldItem == newItem
+    }
+
+}
